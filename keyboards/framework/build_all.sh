e0ed2e12ea8b839455d3764d7b136db1a3c3be79
Phi-Long Do <1766243+philong@users.noreply.github.com>
2026-07-03 18:46:33 +0200

    fl16: Import keyboards/framework from FrameworkComputer fork (fl16-2026-f9)

---
+++ keyboards/framework/build_all.sh
@@ -0,0 +1,7 @@
+set -e
+qmk compile -kb framework/ansi -km default
+qmk compile -kb framework/copilot -km default
+qmk compile -kb framework/iso -km default
+qmk compile -kb framework/jis -km default
+qmk compile -kb framework/numpad -km default
+qmk compile -kb framework/macropad -km default
